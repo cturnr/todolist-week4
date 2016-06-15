405bd34f5358a7bf681906557ad81cda85dc1ea2
Chris Turner <mr.chris.turner@gmail.com>
2016-06-14 23:01:52 -0700

    todo lists are done

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615054359) do
+ActiveRecord::Schema.define(version: 20160615055613) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20160615054359) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
+
+  add_index "users", ["user_id"], name: "index_users_on_user_id"
 
 end
