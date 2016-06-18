2bba2237246c8e018470ab46f9b5df82c0dd5067
Chris Turner <mr.chris.turner@gmail.com>
2016-06-17 17:22:19 -0700

    Validations completed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,23 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_or_last, :male_named_sue
+  validates_inclusion_of :gender, :in => ["male", "female"]
+  # validates_numericality_of :first_name, allow_nil: true
+  # validates_numericality_of :last_name, allow_nil: true
+
+
+def first_or_last
+  if !first_name.present? && !last_name.present?
+    errors.add(:first_name, "cannot have first name and last name be blank")
+  end
+ end
+
+def male_named_sue
+	if "male" == gender &&  "Sue" == first_name
+    errors.add(:first_name, "men cannot be called Sue")
+  end
+end
+
+
 end
