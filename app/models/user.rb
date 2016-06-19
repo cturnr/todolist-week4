39d6e6c967763ed9543f68523acd5ed6985e8d37
Chris Turner <mr.chris.turner@gmail.com>
2016-06-18 22:17:51 -0700

    Completed step 13 to count the completed todo items

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
 	has_many :todo_items, through: :todo_lists, source: :todo_items
 
 	validates :username, presence: true
+
+
+# determines the number of TodoItems the User has completed using an aggregate query function
+# (Hint: You are looking for the count of TodoItems associated with a specific User where
+# completed:true)
+
+def get_completed_count
+  todo_items.where(completed: true).count
 end
+
+end
+
